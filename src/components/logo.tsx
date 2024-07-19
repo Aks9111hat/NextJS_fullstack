f7be8b2d5a2e0791df1916a9e2a8431334cdbf58
Aks9111hat <83699665+Aks9111hat@users.noreply.github.com>
2024-07-20 02:25:37 +0530

    UI enhancements

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,8 +2,8 @@
 
 export default function logo(){
     return(
-        <div className="flex items-center justify-center bg-slate-50 p-1">
-            A <span className="bg-black text-white p-1">S</span>
+        <div className="flex items-center justify-center text-black bg-slate-50 p-1">
+            A <span className="bg-black text-white p-1 rounded">S</span>
         </div>
     )
 }
